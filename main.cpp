ad169ab6de65582ba7b74b7e9308a942bab1c067
Matteo Pompili <matpompili@gmail.com>
2014-08-13 13:19:27 +0200

    Test for realistic simulation

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
   Grid *linTest = new Grid(20, 1.);
   linTest->setAllTemperatures(300.);
-  linTest->setAllAlfas(3);
+  linTest->setAllAlfas(2.3E-5); //Iron
   // for(int i = 0; i < 20; i++)
   // {
   //   linTest->setTemperature(i, 0, 330.);
@@ -18,14 +18,14 @@ int main() {
   //   linTest->setFixed(i, 19, true);
   // }
 
-  Function *func1 = new Function(.1, 30, 0, 300);
+  Function *func1 = new Function(.1, 20, 0, 310);
 
   linTest->setTemperatureFunc(10,10, func1);
-  linTest->setTemperatureFunc(2,2, func1);
-
-  Function *func2 = new Function(.05, 30, M_PI, 300);
-
-  linTest->setTemperatureFunc(15,15, func2);
+  // linTest->setTemperatureFunc(2,2, func1);
+  //
+  // Function *func2 = new Function(.05, 30, M_PI, 300);
+  //
+  // linTest->setTemperatureFunc(15,15, func2);
   //linTest->setTemperature(10, 10, 250.);
   //linTest->setFixed(10, 10, true);
   //linTest->setFixed(19, true);
